4b528dc5a3c3ac240a0001307fec65b61fe54b83
Achilleas Naoumidis <anaumidis@gmail.com>
2016-11-19 13:44:09 +0200

    Merge QueryDslRepository and CustomQueryDslPredicateExecutor

--- sol-domain-querydsl-jpa/src/main/java/org/intellift/sol/domain/querydsl/jpa/repository/QueryDslJpaRepository.java
+++ sol-domain-querydsl-jpa/src/main/java/org/intellift/sol/domain/querydsl/jpa/repository/QueryDslJpaRepository.java
@@ -2,7 +2,7 @@ package org.intellift.sol.domain.querydsl.jpa.repository;
 
 import org.intellift.sol.domain.Identifiable;
 import org.intellift.sol.domain.jpa.repository.JpaRepository;
-import org.intellift.sol.domain.querydsl.executor.CustomQueryDslPredicateExecutor;
+import org.intellift.sol.domain.querydsl.repository.QueryDslRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
@@ -11,5 +11,5 @@ import java.io.Serializable;
  * @author Achilleas Naoumidis, Chrisostomos Bakouras
  */
 @NoRepositoryBean
-public interface JpaQueryDslRepository<T extends Identifiable<ID>, ID extends Serializable> extends JpaRepository<T, ID>, org.intellift.sol.domain.repository.Repository<T, ID>, CustomQueryDslPredicateExecutor<T> {
+public interface QueryDslJpaRepository<T extends Identifiable<ID>, ID extends Serializable> extends QueryDslRepository<T, ID>, JpaRepository<T, ID> {
 }
