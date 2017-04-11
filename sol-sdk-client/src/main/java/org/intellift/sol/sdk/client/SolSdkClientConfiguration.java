5d9e70a9d616178600416960a3f28b0ef8738e9f
Achilleas Naoumidis <anaumidis@gmail.com>
2017-04-11 15:49:56 +0300

    Release 0.4.0

--- sol-sdk-client/src/main/java/org/intellift/sol/sdk/client/SolSdkClientConfiguration.java
+++ sol-sdk-client/src/main/java/org/intellift/sol/sdk/client/SolSdkClientConfiguration.java
@@ -27,22 +27,20 @@ public class SolSdkClientConfiguration {
     @Autowired
     @ConditionalOnBean(RestTemplate.class)
     public void addJavaslangJacksonModuleToRestTemplates(final List<RestTemplate> restTemplates, final JavaslangModule javaslangModule) {
-        restTemplates.forEach(restTemplate -> restTemplate.getMessageConverters()
-                .forEach(converter -> {
-                    if (converter instanceof MappingJackson2HttpMessageConverter) {
-                        ((MappingJackson2HttpMessageConverter) converter).getObjectMapper().registerModule(javaslangModule);
-                    }
-                }));
+        restTemplates.stream()
+                .flatMap(restTemplate -> restTemplate.getMessageConverters().stream())
+                .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
+                .map(MappingJackson2HttpMessageConverter.class::cast)
+                .forEach(converter -> converter.getObjectMapper().registerModule(javaslangModule));
     }
 
     @Autowired
     @ConditionalOnBean(AsyncRestTemplate.class)
     public void addJavaslangJacksonModuleToAsyncRestTemplates(final List<AsyncRestTemplate> asyncRestTemplates, final JavaslangModule javaslangModule) {
-        asyncRestTemplates.forEach(asyncRestTemplate -> asyncRestTemplate.getMessageConverters()
-                .forEach(converter -> {
-                    if (converter instanceof MappingJackson2HttpMessageConverter) {
-                        ((MappingJackson2HttpMessageConverter) converter).getObjectMapper().registerModule(javaslangModule);
-                    }
-                }));
+        asyncRestTemplates.stream()
+                .flatMap(asyncRestTemplate -> asyncRestTemplate.getMessageConverters().stream())
+                .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
+                .map(MappingJackson2HttpMessageConverter.class::cast)
+                .forEach(converter -> converter.getObjectMapper().registerModule(javaslangModule));
     }
 }
